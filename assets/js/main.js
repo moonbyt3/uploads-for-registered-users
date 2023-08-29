17ab3ddbaf3b66e541e2b6fe0c50b70175db49ca
Milorad Jekic <miloradjekic@outlook.com>
2023-08-29 22:40:26 +0200

    Removes dropzone.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,9 @@
 Dropzone.autoDiscover = false;
 
 jQuery(document).ready(function ($) {
-    // var myDropzone = new Dropzone('#my-dropzone', {
-    //     url: '/',
-    //     autoProcessQueue: false, // Disable automatic upload
-    // });
-
-    // $('form').on('submit', function(e) {
-    //     e.preventDefault();
-    //     myDropzone.processQueue(); // Manually trigger file uploads
-    // });
-
     var openButtons = document.querySelectorAll('[js-ufru-open-image]');
 
-
-    // document.getElementById('image').addEventListener('click', function() {
-    //     window.open(this.src, '_blank');
-    // });
-
     if (openButtons.length > 0) {
-
-        console.log(openButtons);
         openButtons.forEach((button) => {
             button.addEventListener('click', (e) => {
                 console.log(e.target, this);
@@ -28,7 +11,6 @@ jQuery(document).ready(function ($) {
                 if (nearestImage) {
                     window.open(nearestImage.src, '_blank');
                 }
-                // window.open(this.src, '_blank');
             });
         });
     }
