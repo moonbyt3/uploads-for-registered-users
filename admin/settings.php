b829f0de2bf6278f97003e36b47d043e490ba9ef
Milorad Jekic <miloradjekic@outlook.com>
2023-10-03 20:38:04 +0200

    Refactors settings page to class, adding allowed formats input

--- admin/settings.php
+++ admin/settings.php
@@ -1,55 +1,150 @@
 <?php
-function settings_page() {
-	add_submenu_page(
-		'uploads-for-registered-users',
-		'Settings',
-		'Settings',
-		'manage_options',
-		'settings',
-		'settings_screen',
-	);
-}
-add_action( 'admin_menu', 'settings_page', 9999 );
+class UFRUSettingsPage
+{
+    private $options;
 
-function ufru_register_settings() {
-    register_setting('ufru-max-number-of-uploads', 'ufru_max_number_of_uploads');
-}
-add_action('admin_init', 'ufru_register_settings');
+    public function __construct() {
+        add_action('admin_menu', [$this, 'add_plugin_settings_page'], 9999);
+        add_action('admin_init', [$this, 'page_init']);
+    }
 
-function settings_screen() {
-    ?>
+    /**
+     * Add options page
+     */
+    public function add_plugin_settings_page() {
+        add_submenu_page(
+            'uploads-for-registered-users',
+            'Settings',
+            'Settings',
+            'manage_options',
+            'ufru-settings-admin',
+            [$this, 'create_admin_page']
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_admin_page() {
+        // Set class property
+        $this->options = get_option( 'ufru_settings' );
+        
+        ?>
         <div class="wrap">
-            <h1>My Plugin Settings</h1>
+            <h1>UFRU <?php _e('Settings', 'uploads-for-registered-users'); ?></h1>
             <form method="post" action="options.php">
-                <?php settings_fields('ufru-max-number-of-uploads'); ?>
-                <table class="form-table">
-                    <tr>
-                        <th scope="row">Max number of user uploads</th>
-                        <td>
-                            <input 
-                                type="number"
-                                name="ufru_max_number_of_uploads" 
-                                id="ufru_max_number_of_uploads"
-                                value="<?php echo urfu_calculate_max_number_of_uploads(); ?>"
-                                min="1"
-                            />
-                        </td>
-                    </tr>
-                </table>
-                <?php submit_button(); ?>
+                <?php
+                    // This prints out all hidden setting fields
+                    settings_fields( 'ufru_general_settings' );
+                    do_settings_sections( 'ufru-settings-admin' );
+                    submit_button();
+                ?>
             </form>
+
+            <div class="wrap">
+                <h2>Debug</h2>
+                <p>
+                    <?php 
+                        $maxFileUploads = ini_get('max_file_uploads'); 
+                        $clientMaxBodySize = ini_get('client_max_body_size');
+                        $debugOptions = get_option( 'ufru_max_number_of_uploads' );
+                    ?>
+                    Server Max File Uploads: <?php echo $maxFileUploads; ?>
+                    <br>
+                    Server Client Max Body Size: <?php echo $clientMaxBodySize; ?>
+                    <br>
+                    Debug options: <?php echo $debugOptions; ?>
+                </p>
+                <br><br>
+                <p>TODO: Scan for deleted users</p>
+                <form method="post">
+                    <button type="submit">Scan</button>
+                </form>
+            </div>
         </div>
-        <div class="wrap">
-            <h2>Debug</h2>
-            <p>
-                <?php $maxUploads = ini_get('max_file_uploads'); ?>
-                INI Max File Uploads: <?php echo $maxUploads; ?>
-            </p>
-            <br><br>
-            <p>TODO: Scan for deleted users</p>
-            <form method="post">
-                <button type="submit">Scan</button>
-            </form>
-        </div>
-    <?php
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init() {        
+        register_setting(
+            'ufru_general_settings', // Option group
+            'ufru_settings', // Option name
+            [$this, 'sanitize'] // Sanitize
+        );
+
+        add_settings_section(
+            'settings_section_general', // ID
+            'General settings', // Title
+            null, // Callback
+            'ufru-settings-admin' // Page
+        );  
+
+        add_settings_field(
+            'ufru_max_number_of_uploads', // ID
+            'Max number of uploads', // Title 
+            [$this, 'input_max_number_of_uploads_callback'], // Callback
+            'ufru-settings-admin', // Page
+            'settings_section_general', // Section
+            [
+                'name' => 'ufru_max_number_of_uploads',
+                'label_for' => 'ufru_max_number_of_uploads',
+            ]
+        );
+
+        add_settings_field(
+            'ufru_upload_file_type', // ID
+            'Allowed formats', // Title
+            [$this, 'input_allowed_formats_callback'], // Callback
+            'ufru-settings-admin', // Page
+            'settings_section_general', // Section
+            [
+                'name' => 'ufru_upload_file_type',
+                'label_for' => 'ufru_upload_file_type',
+            ]
+        );      
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input ) {
+        $new_input = [];
+        if( isset( $input['ufru_max_number_of_uploads'] ) )
+            $new_input['ufru_max_number_of_uploads'] = absint( $input['ufru_max_number_of_uploads'] );
+
+        if( isset( $input['ufru_upload_file_type'] ) )
+            $new_input['ufru_upload_file_type'] = sanitize_text_field( $input['ufru_upload_file_type'] );
+
+        return $new_input;
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function input_max_number_of_uploads_callback() {
+        printf(
+            '<input type="number" id="ufru_max_number_of_uploads" name="ufru_settings[ufru_max_number_of_uploads]" value="%s" />',
+            (isset($this->options['ufru_max_number_of_uploads']) && $this->options['ufru_max_number_of_uploads']) ? esc_attr( $this->options['ufru_max_number_of_uploads']) : 10
+        );
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function input_allowed_formats_callback() {
+        $tip = '<br><small>' . __('Enter file extensions separated by space', 'uploads-for-registered-users') . '</small>';
+        printf(
+            '<input type="text" id="ufru_upload_file_type" name="ufru_settings[ufru_upload_file_type]" value="%s" />',
+            (isset( $this->options['ufru_upload_file_type'] ) && !empty($this->options['ufru_upload_file_type'])) ? esc_attr( $this->options['ufru_upload_file_type']) : '.jpg .jpeg .png'
+        );
+        print($tip);
+    }
 }
+
+if( is_admin() )
+    $my_settings_page = new UFRUSettingsPage();
